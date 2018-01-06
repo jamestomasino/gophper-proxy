51f49d8aa4d29ae21f6dab98bd119a53b6213c48
James Tomasino <james@tomasino.org>
2018-01-05 21:26:28 -0500

    made paths relative so things actually load

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
 // a Google Analytics account -- if specified, we will log activity
-//define('GA_ACCOUNT', 'UA-XXXXXXX-X');
+define('GA_ACCOUNT', 'UA-XXXXXXX-X');
 
 // should we allow users to connect on any port? If false, only allow port 70. If true,
 // we allow port 70, AND any port greater than 1024, since anything below that is reserved.
 define('ALLOW_ALL_PORTS', true);
 
 // an optional regex to restrict hosts too -- if a host doesn't match this, we won't process the request
-define('RESTRICT_TO_MATCH', "/sdf.org/");
+//define('RESTRICT_TO_MATCH', "/sdf.org/");
 
 // lifetime of requests in the cache
 define('CACHE_LIFETIME', 3600);
@@ -17,14 +17,14 @@ define('CACHE_PATH', "/tmp");
 define('APP_NAME', 'Gophper');
 
 // default request to serve up on the index page
-//define('START_REQUEST', 'gopherpedia.com/');
-//define('START_INPUT', '');
+define('START_REQUEST', 'sdf.org');
+define('START_INPUT', '');
 
 // maximum filesize allowed when browsing/downloading files, etc
 define('MAX_FILESIZE', 10000000);
 
 // should we log stats to the db?
-//define('LOG_STATS', true);
+define('LOG_STATS', false);
 
 // if LOG_STATS is true, this specifies the connection to the database
 //DB::$user = '';
--- lib/GopherGetter.php
+++ lib/GopherGetter.php
@@ -5,7 +5,7 @@ require 'Cache.php';
    todo:
    - limit download size
    - santize input
-*/
+ */
 
 class GopherGetter {
   public $uri;
@@ -19,73 +19,75 @@ class GopherGetter {
   private $cache;
 
   function __construct($u, $i = NULL) {
-		$this->cache = new Cache(CACHE_PATH);  //Make sure it exists and is writeable
+    $this->cache = new Cache(CACHE_PATH);  //Make sure it exists and is writeable
 
-		// split the incoming URI on :// to prevent too much fooling 
-		// around and to make sure that we normalize our data
-		$u = array_pop(explode("://", $u));
-
-
-		// strip any leading slash from the path
-	  $u = ltrim($u, '/');
-		$this->uri = "gopher://$u";
-
-		// split host and port from uri
-		$data = parse_url($this->uri);
-
-		$this->host = $data['host'];
-		$this->port = array_key_exists('port', $data) ? $data['port'] : 70;
-
-		// only allow port 70, or if ALLOW_ALL_PORTS is true, also allow ports over 1024
-		if ( $this->port != 70 && ( (defined('ALLOW_ALL_PORTS') && ALLOW_ALL_PORTS != true) || $this->port < 1024 ) ) {
-		  throw new Exception("Port violation");
-		}
-
-		if ( defined('RESTRICT_TO_MATCH') && ! preg_match(RESTRICT_TO_MATCH, $this->host) ) {
-		  throw new Exception("Host violation");
-		}
+    // split the incoming URI on :// to prevent too much fooling
+    // around and to make sure that we normalize our data
+    if (strpos($u, '://') !== false) {
+      $u = array_pop(explode("://", $u));
+    }
 
 
-		// strip the leading slash from the path
-		if ( array_key_exists('path', $data) ) {
-		  $this->path = ltrim($data['path'], '/');
-		}
-		else {
-		  $this->path = "/";
-		}
+    // strip any leading slash from the path
+    $u = ltrim($u, '/');
+    $this->uri = "gopher://$u";
 
-		$this->path = urldecode($this->path);
-		$this->input = $i;
+    // split host and port from uri
+    $data = parse_url($this->uri);
 
-		$this->key = "$this->host:$this->port$this->path?$this->input";
+    $this->host = $data['host'];
+    $this->port = array_key_exists('port', $data) ? $data['port'] : 70;
+
+    // only allow port 70, or if ALLOW_ALL_PORTS is true, also allow ports over 1024
+    if ( $this->port != 70 && ( (defined('ALLOW_ALL_PORTS') && ALLOW_ALL_PORTS != true) || $this->port < 1024 ) ) {
+      throw new Exception("Port violation");
+    }
+
+    if ( defined('RESTRICT_TO_MATCH') && ! preg_match(RESTRICT_TO_MATCH, $this->host) ) {
+      throw new Exception("Host violation");
+    }
+
+
+    // strip the leading slash from the path
+    if ( array_key_exists('path', $data) ) {
+      $this->path = ltrim($data['path'], '/');
+    }
+    else {
+      $this->path = "/";
+    }
+
+    $this->path = urldecode($this->path);
+    $this->input = $i;
+
+    $this->key = "$this->host:$this->port$this->path?$this->input";
   }
 
   function isValid() {
-		return isSet($this->host) && isSet($this->port) && isSet($this->path);
+    return isSet($this->host) && isSet($this->port) && isSet($this->path);
   }
 
 
   function isBinary() {
-		return $this->cache->isBinary($this->key);
+    return $this->cache->isBinary($this->key);
   }
 
   function isImage() {
-		return $this->cache->isImage($this->key);
+    return $this->cache->isImage($this->key);
   }
 
   function urlFor() {
-		return $this->cache->url($this->key);
+    return $this->cache->url($this->key);
   }
 
   function get() {
 
-		$this->result = "";
-		$this->errstr = "";
-		$this->errno = "";
+    $this->result = "";
+    $this->errstr = "";
+    $this->errno = "";
 
-		$this->result = $this->cache->get($this->key);
-		if ( $this->result === FALSE ) {
-      try { 
+    $this->result = $this->cache->get($this->key);
+    if ( $this->result === FALSE ) {
+      try {
         $fp = stream_socket_client("tcp://$this->host:$this->port", $this->errno, $this->errstr, 30);
       }
       catch(Exception $e) {
@@ -93,20 +95,20 @@ class GopherGetter {
         return FALSE;
       }
 
-		  if (!$fp) {
-				return FALSE;
-	  	}
-	  	else {
-			  $data = $this->path;
-			  if ( isset($this->input) && $this->input != "" ) {
+      if (!$fp) {
+        return FALSE;
+      }
+      else {
+        $data = $this->path;
+        if ( isset($this->input) && $this->input != "" ) {
           $data .= "\t$this->input";
-		  	}
+        }
 
-				fwrite($fp, "$data\r\n");
+        fwrite($fp, "$data\r\n");
         $size = 0;
-				while (!feof($fp)) {
+        while (!feof($fp)) {
           $buf = fread($fp, 1024);
-				  $this->result .= $buf;
+          $this->result .= $buf;
           $size += strlen($buf);
 
           if ( defined('MAX_FILESIZE') && $size > MAX_FILESIZE ) {
@@ -115,36 +117,36 @@ class GopherGetter {
             return FALSE;
           }
 
-				}
-				fclose($fp);
-	  	}
+        }
+        fclose($fp);
+      }
 
 
-		  $this->cache->set($this->key, $this->result);
-		}
+      $this->cache->set($this->key, $this->result);
+    }
 
-		$this->logTraffic($this->host, $this->path);
-		return TRUE;
+    $this->logTraffic($this->host, $this->path);
+    return TRUE;
   }
 
   function size() {
-		return $this->cache->size($this->key);
+    return $this->cache->size($this->key);
   }
 
   function logTraffic($host, $selector) {
-		if ( ! defined('LOG_STATS') || LOG_STATS != true ) {
-		  return;
-		}
+    if ( ! defined('LOG_STATS') || LOG_STATS != true ) {
+      return;
+    }
 
-		$ip_address = ip2long($_SERVER['REMOTE_ADDR']);
+    $ip_address = ip2long($_SERVER['REMOTE_ADDR']);
 
-		DB::insert('traffic', array(
-                                'hostname' => $host,
-                                'selector' => $selector,
-                                'remote_ip' => $ip_address,
-                                'filesize' => $this->size(),
-                                'request_at' => DB::sqleval("NOW()")
-                                ));
+    DB::insert('traffic', array(
+      'hostname' => $host,
+      'selector' => $selector,
+      'remote_ip' => $ip_address,
+      'filesize' => $this->size(),
+      'request_at' => DB::sqleval("NOW()")
+    ));
 
   }
 };
